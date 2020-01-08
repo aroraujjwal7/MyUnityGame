64b1a904960b2f4a300cbcf01e54783251256939
Ujjwal Arora <aroraujjwal7@gmail.com>
2020-01-09 02:19:25 +0530

    Make Camera follow

--- MyFirstGame/Assets/PlanePilot.cs
+++ MyFirstGame/Assets/PlanePilot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlanePilot : MonoBehaviour
 {
+    float speed = 90.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,25 @@ public class PlanePilot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+      Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 5.0f;
+      Camera.main.transform.position = moveCamTo;
+
+      Camera.main.transform.LookAt( transform.position );
+
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+        speed -= transform.forward.y * Time.deltaTime * 50.0f;
+
+        if(speed < 35.0f){
+          speed = 35.0f;
+        }
+
         transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+        float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight( transform.position );
+        if(terrainHeightWhereWeAre > transform.position.y){
+          transform.position = new Vector3(transform.position.x,
+          terrainHeightWhereWeAre, transform.position.z);
+        }
     }
 }
