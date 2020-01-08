1595b4fc7fe6df1d14b28db1c92ef310831dc485
Ujjwal Arora <aroraujjwal7@gmail.com>
2020-01-09 00:21:23 +0530

    Initial Commit
    Partial Start to Game

---
+++ MyFirstGame/Assets/PlanePilot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanePilot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("plane pilot script added to:" + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+    }
+}
